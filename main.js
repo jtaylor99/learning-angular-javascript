398be0cae058ef5e10c5e2ac21d67567b6655031
jtaylor99 <jtaylor@basecampcodingacademy.org>
2018-11-04 19:16:06 -0600

    Example 3 plus notes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope) {
-    $scope.firstname= "";
-    $scope.lastname= "";
-});
+// var app = angular.module("myApp", []);
+// app.controller("myCtrl", function($scope) {
+//     $scope.firstname= "";
+//     $scope.lastname= "";
+// });
